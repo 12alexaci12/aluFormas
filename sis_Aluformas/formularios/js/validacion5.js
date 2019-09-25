a41afe4f67b6de546be0df8472a1848ca0d93e6e
12alexaci12 <12alexaci12@gmail.com>
2019-09-24 20:01:02 -0400

    cambio en validacion5 quitamos linea

--- sis_Aluformas/formularios/js/validacion5.js
+++ sis_Aluformas/formularios/js/validacion5.js
@@ -2,19 +2,12 @@
 
 function validar() {
     var nombres     = document.form.nombres.value;
-    var linea       = document.form.linea.value;
     var precio      = document.form.precioM2.value;
 
 
     if(nombres == ""){
         alert("el campo nombres esta vacio");
         document.form.nombres.focus();
-        return;
-    }
-
-    if (!v2.test(linea) || linea == "") {
-        alert("el campo de linea es incorrecto o esta vacio")
-        document.form.linea.focus();
         return;
     }
 
